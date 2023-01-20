95b60bdeffc79657f5ce7ac7e09aa6f8157ebf57
Andrii Dobronos <andriidobronos@gmail.com>
2023-01-20 10:33:59 +0200

    corrected validation

--- NumbersRows.js
+++ NumbersRows.js
@@ -1,31 +1,50 @@
 "use strict";
 //Отримати від користувача 3 рядки та вивести їх
 //у довільному порядку однією командою (конкатенація);
-const firstArray = prompt('Enter first row items for' +
-    ' array separate them with a space').split(" ")
-const secondArray = prompt('Enter second row items for' +
-    ' array separate them with a space').split(" ")
-const thirdArray = prompt('Enter third row items for' +
-    ' array separate them with a space').split(" ")
+
+function firstString1(){
+    let firstString1 = prompt('Input first string ')
+    while (!firstString1?.trim()) {
+        firstString1 = prompt('You have not inputted the first string, repeat please')
+    }
+    return firstString1
+}
+const firstString =  firstString1()
+function secondString1(){
+    let secondString1 = prompt('Input second string ')
+    while (!secondString1?.trim()) {
+        secondString1 = prompt('You have not inputted the second string, repeat please')
+    }
+    return secondString1
+}
+const secondString =  secondString1()
+function thirdString1(){
+    let thirdString1 = prompt('Input third string ')
+    while (!thirdString1?.trim()) {
+        thirdString1 = prompt('You have not inputted the third string, repeat please')
+    }
+    return thirdString1
+}
+const thirdString =  thirdString1()
 const randomNum = Math.random()*6
 let result
 if (randomNum > 0 && randomNum < 1) {
-    result = `${firstArray}\n ${secondArray}\n ${thirdArray}`
+    result = `${firstString}\n ${secondString}\n ${thirdString}`
 }
 else if (randomNum > 1 && randomNum < 2) {
-    result = `${secondArray}\n ${firstArray}\n ${thirdArray}`
+    result = `${secondString}\n ${firstString}\n ${thirdString}`
 }
 else if(randomNum > 2 && randomNum < 3) {
-    result = `${thirdArray}\n ${firstArray}\n ${secondArray}`
+    result = `${thirdString}\n ${firstString}\n ${secondString}`
 }
 else if (randomNum > 3 && randomNum < 4) {
-    result = `${firstArray}\n ${thirdArray}\n ${secondArray}`
+    result = `${firstString}\n ${thirdString}\n ${secondString}`
 }
 else if (randomNum > 4 && randomNum < 5) {
-    result = `${secondArray}\n ${thirdArray}\n ${firstArray}`
+    result = `${secondString}\n ${thirdString}\n ${firstString}`
 }
 else {
-    result = `${thirdArray}\n ${secondArray}\n ${firstArray}`
+    result = `${thirdString}\n ${secondString}\n ${firstString}`
 }
 const definition = `Array of rows in random oder :\n`
 alert(definition.concat(` ${result}`))
